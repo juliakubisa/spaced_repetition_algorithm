4bd679cb84ae5bca4690ae8e547214716497dee9
Julka Kubisa <julkakubisa@gmail.com>
2025-03-06 12:31:50 +0100

    add neural network model

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scripts.file_storage import FileStorage
 from scripts.feature_extraction.user_features import UserFeatures
 from scripts.feature_extraction.language_features import LanguageFeatures
 from scripts.models.LR import LinearRegression
+from scripts.models.neural_network import NeuralNetwork
 
 def main():
     RawDataProcessor().process_data('13 million Duolingo student learning traces.csv', 'raw', 3, 'df_processed.csv', 'processed_test')
@@ -20,6 +21,8 @@ def main():
     linear_regression.evaluate(y_train_pred, y_test_pred, include_importance=False)
     linear_regression.residuals_histogram(y_test_pred)
     linear_regression.predictions_scatterplot(y_test_pred)
+
+    neural_network = NeuralNetwork(data, user_data, language_data, 0.4)
 
     # input_df = PrepareDataset.concat_datasets(processed_df, user_features, language_features)
     # trainset, testset =  PrepareDataset.create_instances_from_dataframe(input_df)
